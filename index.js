3c5847e2c1396338a023320b28fc15fea3fb3f36
SaAbdullah131 <cse.asabdullah@gmail.com>
2022-12-13 21:02:43 +0600

    Button Ripple Effects javascript part done

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-body{
-    margin:0;
-    display:flex;
-    
-}
+const buttonEl=document.querySelector(".btn");
+
+buttonEl.addEventListener("mouseover",(event)=>{
+const x=(event.pageX-buttonEl.offsetLeft);
+const y=(event.pageY-buttonEl.offsetTop);
+
+buttonEl.style.setProperty("--xPos",x+ "px");
+
+buttonEl.style.setProperty("--yPos",y+"px");
+
+});
